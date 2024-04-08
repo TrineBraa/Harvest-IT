15bae906b94bb29ad9389ad3cee5f505b31a6193
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-04-08 13:52:15 +0200

    Co-authored-by: EvenPB <EvenPB@users.noreply.github.com>

---
+++ JS/Views/main.js
@@ -0,0 +1,10 @@
+mainPageView()
+function mainPageView() {
+    document.getElementById("app").innerHTML = /*HTML*/ `
+    <input type="text" id="searchbar">
+    <div id="Profile" onclick="OpenProfile">(Profile)</div> </br>
+   <button id="Berries" onclick="clickBerriesf"()>Berries</button>
+   <button id="Mushrooms" onclick="clickMushroomsf"()>Mushrooms</button>
+   <button id="Fish" onclick="ClickFishf"()>Fish</button> 
+   <button id="Register New Find" onclick="registerNewFind"()>Register New Find</button>  `
+}
