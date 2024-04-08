7ce9187f8583dc0ee96254ed0585f2daf79e0eb7
trinebra <157688079+trinebra@users.noreply.github.com>
2024-04-08 13:45:53 +0200

    controller links

---
+++ JS/Views/login.js
@@ -0,0 +1,22 @@
+logInView()
+
+
+function logInView(){
+    document.getElementById("app").innerHTML= /*HTML*/ `
+    <h1>Harvest IT</h1>
+    <br/>
+    <img/>
+    <br/>
+    <br/>
+    <div id="logInInputContainer">
+    <input/>
+    <br/>
+    <input/>
+    </div>
+    <br/>
+    <br/>
+    <button>Log In</button>
+    <button>Register New User</button>
+    <br/>
+    `;
+}
