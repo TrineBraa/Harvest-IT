03d5a9beb945c650db984cf303ec1540f62b8cca
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-04-10 11:32:14 +0200

    Co-authored-by: EvenPB <EvenPB@users.noreply.github.com>

---
+++ JS/Controller/maincontrol.js
@@ -0,0 +1,2 @@
+fetch("https://raw.githubusercontent.com/EvenPB/Harvest-Modell/main/model.js").then(res => res.json()).then
+(data => {})
--- JS/Views/main.js
+++ JS/Views/main.js
@@ -1,12 +1,23 @@
 
+mainPageView()
 function mainPageView() {
     document.getElementById('app').innerHTML = /*HTML*/ `
-    <input type="text" id="searchbar" placeholder="Search in users, berries, mushrooms">
+    Du er logget inn som ${model.app.user}
+    <div class="search-wrapper">
+    <label for="search"> Search Bar</label>
+    <input type="search" id="searchUsers" placeholder="Search in users">
+    </div>
+    <div class="user-cards">
+    <div class="card">
+    <div class="header">Navn</div>
+    <div class="body">Email/Informasjon</div>
+    </div>
+    </div>
     <button id="Profile" onclick="OpenProfile()">(Profile)</button> </br>
    <button id="Berries" onclick="clickBerriesf()">Berries</button>
    <button id="Mushrooms" onclick="clickMushroomsf()">Mushrooms</button>
    <button id="Fish" onclick="ClickFishf()">Fish</button>  </br>
-   <button id="Register New Find" onclick="registerNewFind()">Register New Find</button>  `;
+   <button id="RegisterNewFind" onclick="registerNewFindf()">Register New Find</button>  `;
 }
 
 function OpenProfile() {
@@ -24,6 +35,6 @@ function clickMushroomsf() {
 function ClickFishf() {
     fishview();
 }
-function registerNewFind() {
+function registerNewFindf() {
     registerViewF();
 }
