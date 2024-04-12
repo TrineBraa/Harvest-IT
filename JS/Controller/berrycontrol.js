46388fc641ef2df83019e1db06cdc45b49f4ab44
trinebra <157688079+trinebra@users.noreply.github.com>
2024-04-12 13:54:36 +0200

    .

--- JS/Controller/berrycontrol.js
+++ JS/Controller/berrycontrol.js
@@ -1,7 +1,3 @@
-function returnButtonMain(){
-    let html = `<div id="closeButton"><button id="returnButton" onclick="mainPageView()">X</button></div>`;
-    return html;
-}
 
 function returnButtonBerry(){
     let html = `<div id="closeButton"><button id="returnButton" onclick="berryView()">X</button></div>`;
