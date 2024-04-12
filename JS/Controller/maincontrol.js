ffabc5291d899a8695eeed359f92bb45aef5039b
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-04-12 16:11:14 +0200

    FinishWeek3

--- JS/Controller/maincontrol.js
+++ JS/Controller/maincontrol.js
@@ -1,8 +1,40 @@
-fetch("https://raw.githubusercontent.com/EvenPB/Harvest-Modell/main/model.js").then(res => res.json()).then
-(data => {})
+let availableKeywords = [
+    'Fish',
+    'Mushrooms',
+    'Berries',
+    'Map',
+    'Profile',
+    'Help',
+];
 
+const resultsBox = document.querySelector(".results-box");
+const inputBox = document.querySelector("input-box");
 
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result= availableKeywords.filter((keyword)=>{
+          return  keyword.toLowerCase().includes(input.toLowerCase());
+    });
+    console.log(result);
+    }
+    display(result);
+    if(!result.length) {
+        resultsBox.innerHTML = '';
+    }
 
+};
+function display(result){
+    const content = result.map((list) => {
+        return "<li onclick=selectInput(this)> + list + </li>";
+    });
+    resultsBox.innerHTML = "<ul" + content.join('') + "</ul";
+}
+function selectInput(list) {
+    inputBox.value = list.innerHTML
+    resultsBox.innerHTML = '';
+}
 
 function returnButtonMain(){
     let html = `<div id="closeButton"><button id="returnButton" onclick="mainPageView()">X</button></div>`;
--- JS/Views/main.js
+++ JS/Views/main.js
@@ -3,26 +3,30 @@
 function mainPageView() {
     document.getElementById('app').innerHTML = /*HTML*/ `
     Du er logget inn som ${model.app.loggedInUser}
-    <button id="Profile" onclick="OpenProfile()">(Profile)</button>
-    <div class="search-wrapper">
-    <label for="search"> Search Bar</label>
-    <input type="search" id="searchUsers" placeholder="Search in users">
-    </div>
-    <div class="user-cards">
-    <div class="card">
-    <div class="header">Name</div>
-    <div class="body">Email/Information</div>
-    <br/>
-    <img id="mushroomLogin" src="./Images/mushroomlogo.png">
-    </div>
-    </div>
-    <br/>
-   <button id="Berries" onclick="clickBerriesf()">Berries</button>
-   <button id="Mushrooms" onclick="clickMushroomsf()">Mushrooms</button>
-   <button id="Fish" onclick="ClickFishf()">Fish
-    <img class="fishypic" src="/Images/FishBtn.png">
-   </button>  <br/>
-   <button id="RegisterNewFind" onclick="registerNewFindf()">Register New Find</button>  `;
+    <div id="mainContainer">
+        <button id="Profile" onclick="OpenProfile()">(Profile)</button>
+        <div class="search-wrapper">
+            <div class="row">
+                <input type="text" id="input-box" placeholder="Search for Activities" autocomplete="off">
+                <button id="Search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <div class="result-box">
+                </div>
+            </div>
+        </div>
+        <br/>
+        <button id="Berries" onclick="clickBerriesf()">Berries
+            <img class="berryPic" src="/Images/berries.png">
+        </button>
+        <button id="Mushrooms" onclick="clickMushroomsf()">Mushrooms
+            <img class="mushyPic" src="/Images/shrooms.png">
+        </button>
+        <button id="Fish" onclick="ClickFishf()">Fish
+            <img class="fishypic" src="/Images/FishBtn.png">
+        </button>  <br/>
+        <button id="RegisterNewFind" onclick="registerNewFindf()">Register New Find
+        <img class="registerPlus" src="/Images/Register.png">
+        </button>
+   </div> `;
 }
 
 function OpenProfile() {
